4ea9aa053aacf54329fee9a5f0c6874f911b5b92
laitheyad <laithop@gmail.com>
2020-03-07 21:09:24 +0200

    Api update

---
+++ common/indexes/index.js
@@ -0,0 +1,8 @@
+
+  const links={
+    SubjectsApi:'http://laitheyad1.pythonanywhere.com/subjects/',
+    ArticlesApi:'http://laitheyad1.pythonanywhere.com/articles/',
+
+    }
+
+export default links;
