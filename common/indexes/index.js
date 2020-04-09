4092ab2b9ba816d4300bd9ae3ddde320a882e9b2
laitheyad <laithop@gmail.com>
2020-04-09 14:51:12 +0300

    subjects update

--- common/indexes/index.js
+++ common/indexes/index.js
@@ -2,6 +2,7 @@
   const links={
     SubjectsApi:'http://laitheyad1.pythonanywhere.com/subjects/',
     ArticlesApi:'http://laitheyad1.pythonanywhere.com/articles/',
+    weatherAPI:'http://api.openweathermap.org/data/2.5/weather?q=Amman&appid=49767227024363fbebe8536971184160',
 
     }
 
